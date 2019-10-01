262113d86f448dce86c8b9e9a3ac1869cdcd2bfa
davidtenorio1 <dtenorio2169@gmail.com>
2019-10-01 10:37:48 -0500

    updates

--- 4.9_seaborn_exercises.py
+++ 4.9_seaborn_exercises.py
@@ -17,12 +17,12 @@ sns.boxplot(data=iris.petal_length)
 sns.relplot(data=iris, x='petal_length', y='petal_width')
 #Yes
 # Would it be reasonable to predict species based on sepal width and sepal length?
-sns.relplot(data=iris, x='petal_length', y='petal_width', col = 'species')
-#Yes
+sns.relplot(data=iris, x='sepal_length', y='sepal_width', hue = 'species')
+#Yes, but it would be not be entirely accurate
 # Which features would be best used to predict species?
 sns.relplot(data=iris, x='petal_length', y='petal_width', hue = 'species')
 sns.relplot(data=iris, x='sepal_length', y='sepal_width', hue = 'species')
-sns.pairplot(iris)
+sns.pairplot(data = iris, hue = 'species')
 
 
 
@@ -51,14 +51,14 @@ swiss_df = pd.DataFrame(swiss)
 
 # Create an attribute named is_catholic that holds a boolean value of whether or not the province is Catholic. 
 # (Choose a cutoff point for what constitutes catholic)
-swiss_df['is_catholic'] = swiss_df.Catholic >= 65
+swiss_df['is_catholic'] = swiss_df.Catholic >= 50
 
 # Does whether or not a province is Catholic influence fertility?
 # Yes
-sns.lmplot(data = swiss_df, x = 'is_catholic', y = 'Fertility' )
+sns.boxplot(data = swiss_df, x = 'is_catholic', y = 'Fertility' )
 
 # What measure correlates most strongly with fertility?
-swiss.corr()
+swiss.corr().Fertility
 sns.heatmap(data=swiss.corr(), cmap = 'GnBu')
 
 # Using the chipotle dataset from the previous exercise, create a bar chart that shows the 4 most popular items and the revenue produced by each.
@@ -66,23 +66,31 @@ def get_db_url(user, password, host, database_name):
     url = f'mysql+pymysql://{user}:{password}@{host}/{database_name}'
     return url
 chipotle_url = get_db_url(user,password,host,'chipotle')
-popular_items = pd.read_sql('select item_name, count(item_name) from orders group by item_name order by count(item_name) desc limit 4', chipotle_url)
-item_prices = pd.read_sql('select item_name, item_price from orders order by item_name', chipotle_url)
-item_prices['clean_item_prices'] = item_prices.item_price.str.replace('$','').astype(float)
-sum_item_prices = item_prices.groupby('item_name').sum()
-popular_items['revenue']= sum_item_prices['clean_item_prices']
+query = 'SELECT * FROM orders'
+orders = pd.read_sql(query, chipotle_url)
+orders.head()
+orders.item_price = orders.item_price.str.replace('$', '').astype('float')
+four_most_popular = (orders.groupby('item_name')
+ .sum()
+ .drop(columns='order_id')
+ .rename(columns={'quantity': 'units_sold', 'item_price': 'revenue'})
+ .sort_values(by='units_sold', ascending=False)
+ .head(4)
+ .reset_index()
+)
+
+sns.barplot(data=four_most_popular, y='revenue', x='item_name')
+plt.ylabel('')
+plt.xticks()
 
 
 
 # Load the sleepstudy data and read it's documentation. 
 # Use seaborn to create a line chart of all the individual subject's reaction times and a more prominant line showing the average change in reaction time.
-sleepstudy = data('sleepstudy')
-study = sns.lineplot(x='Days', y= 'Reaction', data = sleepstudy, size = 'Subject', hue = 'Subject', palette = 'jet')
-
-
-
-
-
+sleep = data('sleepstudy')
+sleep.Subject = 'subject_' + sleep.Subject.astype(str)
+sns.lineplot(data=sleep, y='Reaction', x='Days', hue='Subject', alpha=.4)
+sns.lineplot(data=sleep, y='Reaction', x='Days', color='navy')
 
 
 
